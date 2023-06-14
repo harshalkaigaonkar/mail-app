4cc8e049fbfec0223764dad6c89aea310c94c4b1
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2023-06-14 18:54:51 +0530

    fix:sidebar active for All Mails

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -42,7 +42,7 @@ const Sidebar = () => {
   return (
     <div className='py-4 sticky top-20 left-0 list-none h-[90vh] flex flex-col justify-start gap-2 border-r w-2/12'>
         {sidebar.map((item, index) => (
-            <li key={index} id={item.type}  className={`${item.type.toLowerCase() === pathname.split("/")[2] ? "bg-black/10 ": ""}
+            <li key={index} id={item.type}  className={`${(item.type === "All" && pathname === "/") || item.type.toLowerCase() === pathname.split("/")[2] ? "bg-black/10 ": ""}
             w-full p-1 flex gap-4 items-center justify-center pr-2 w-[80%] rounded-r-md`} onClick={onClickHandler}>
                 <img src={`${item.icon}`} alt={`${item.type}`} className="w-[20px] h-[20px]" /> 
                 <p className='font-semibold w-12'>{item.type}</p>
